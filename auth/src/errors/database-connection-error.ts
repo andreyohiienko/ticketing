f735140cd83c6298c7f8860a6e6538dde653fe28
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-14 18:00:52 +0300

    Final error related code

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,13 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from './custom-error'
+
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500
   reason = 'Error connection to database'
 
   constructor() {
-    super()
+    super('Error connection to database')
 
+    // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
   }
 
