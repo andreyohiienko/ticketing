b401b0911047a7673ffd61e98ae70609116d174f
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-16 11:18:37 +0300

    Comparing hashed password

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -4,12 +4,17 @@ import { promisify } from 'util'
 const scryptAsync = promisify(scrypt)
 
 export class Password {
-  static toHash(password: string) {
+  static async toHash(password: string) {
     const salt = randomBytes(8).toString('hex')
     const buf = (await scryptAsync(password, salt, 64)) as Buffer
 
     return `${buf.toString('hex')}.${salt}`
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.')
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer
+
+    return buf.toString('hex') === hashedPassword
+  }
 }
