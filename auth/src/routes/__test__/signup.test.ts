821dcccfe07df70d39dc5728826a2a8a8e50921a
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-17 13:11:17 +0300

    Testing invalid input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,34 @@ it('returns a 201 on successful signupo', async () => {
     })
     .expect(201)
 })
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'p',
+    })
+    .expect(400)
+})
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400)
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'asdfasdf' })
+    .expect(400)
+})
