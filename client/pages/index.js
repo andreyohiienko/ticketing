2e0ab3a7656ec9532bac0079a6e8f8796be1e55b
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-18 12:48:56 +0300

    On the server or the browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,6 +8,13 @@ LandingPage.getInitialProps = async () => {
   // const response = await axios.get('/api/users/currentuser')
 
   // return response.data
+  if (typeof window === 'undefined') {
+    // we are on the server
+    // request should be made to http://ingress-nginx.ingress-nginx-controller...asasghgjl
+  } else {
+    // we are on the browser!
+    // requests can be made with abaswe url of ''
+  }
   console.log('I was executed')
   return {}
 }
