d28109ffc950fa5f3e925f00434c06290b039b65
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-18 12:45:32 +0300

    When is getInitialProps called

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,15 @@
 import axios from 'axios'
 
 const LandingPage = ({ currentUser }) => {
-  console.log('currentUser', currentUser)
   return <h1>Landing page</h1>
 }
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser')
+LandingPage.getInitialProps = async () => {
+  // const response = await axios.get('/api/users/currentuser')
 
-//   return response.data
-// }
+  // return response.data
+  console.log('I was executed')
+  return {}
+}
 
 export default LandingPage
