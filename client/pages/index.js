0193843c3816a600e75c26ac5c7cd9310182f0b0
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-18 13:09:33 +0300

    Specifying the Host & Passing through the cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,28 @@
 import axios from 'axios'
 
 const LandingPage = ({ currentUser }) => {
+  console.log('currentUser', currentUser)
   return <h1>Landing page</h1>
 }
 
-LandingPage.getInitialProps = async () => {
-  // const response = await axios.get('/api/users/currentuser')
-
-  // return response.data
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     // request should be made to http://ingress-nginx.ingress-nginx-controller...asasghgjl
+    // http://SERVICENAME.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      },
+    )
+    return data
   } else {
     // we are on the browser!
     // requests can be made with abaswe url of ''
+    const { data } = await axios.get('/api/users/currentuser')
+
+    return data
   }
   console.log('I was executed')
   return {}
