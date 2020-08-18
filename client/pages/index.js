ea661b3d845da49fef9968b9a690bd7c52471761
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-18 18:26:28 +0300

    Remove console.log()

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,8 +9,6 @@ const LandingPage = ({ currentUser }) => {
 }
 
 LandingPage.getInitialProps = async (context) => {
-  console.log('LANDING PAGE')
-
   const client = buildClient(context)
 
   const { data } = await client.get('/api/users/currentuser')
