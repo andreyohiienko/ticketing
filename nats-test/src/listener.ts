232a0b3df3b2daa6ac0ba75dafe26ae9accd0734
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-20 18:19:12 +0300

    Graceful client shutdown

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,6 +10,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to nats')
 
+  stan.on('close', () => {
+    console.log('NATS connection closed')
+    process.exit()
+  })
+
   const options = stan.subscriptionOptions().setManualAckMode(true)
   const subscription = stan.subscribe(
     'ticket:created',
@@ -26,3 +31,6 @@ stan.on('connect', () => {
     msg.ack()
   })
 })
+
+process.on('SIGINT', () => stan.close()) // INTERRUPT SIGNALS
+process.on('SIGTERM', () => stan.close()) // TERMINATE SIGNALS
