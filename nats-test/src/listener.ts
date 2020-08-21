c06d4102793f71913141275448da30a8c47af715
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-21 12:34:46 +0300

    Extending the Listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,26 +15,7 @@ stan.on('connect', () => {
     process.exit()
   })
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable() // 1
-    .setDurableName('accounting-service') // 2
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name', // 3 - to make sure that we do not accidentlly dumped durable name even if all of our services restart for a very brief period of time
-    options,
-  )
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData()
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-    }
-
-    msg.ack()
-  })
+  new TicketCreatedListener(stan).listen()
 })
 
 process.on('SIGINT', () => stan.close()) // INTERRUPT SIGNALS
@@ -80,5 +61,16 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf-8'))
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created'
+  queueGropName = 'payments-service'
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data)
+
+    msg.ack()
   }
 }
