bbb695b6a39d6d173381a0c05a451e25c790e307
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-21 17:44:11 +0300

    Add readonly

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,7 +4,7 @@ import { TicketCreatedEvent } from './ticket-created-event'
 import { Subjects } from './subjects'
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated
   queueGropName = 'payments-service'
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {
