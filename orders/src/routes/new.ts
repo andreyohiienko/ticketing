175149905c38afe296a6d5971f4fb763652fd924
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-23 11:08:12 +0300

    Order creation logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
-import { validateRequest } from '@aotickets/common'
+import { validateRequest, NotFoundError } from '@aotickets/common'
+import { Ticket } from '../models/ticket'
 
 const router = express.Router()
 
@@ -16,6 +17,22 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body
+
+    // Find the ticket the user is tying to order in the database
+    const ticket = await Ticket.findById(ticketId)
+    if (!ticket) {
+      throw new NotFoundError()
+    }
+
+    // Make sure that this ticket not already reserved
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({})
   },
 )
