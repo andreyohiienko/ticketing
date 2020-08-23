bcf9cba2ac1ce8260b1b2e97aa4cde6d5aa85459
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-23 11:14:45 +0300

    Finding reserved ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
-import { validateRequest, NotFoundError } from '@aotickets/common'
+import {
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from '@aotickets/common'
 import { Ticket } from '../models/ticket'
+import { Order } from '../models/order'
 
 const router = express.Router()
 
@@ -26,6 +32,22 @@ router.post(
     }
 
     // Make sure that this ticket not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* canceled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    })
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is alredy reserved')
+    }
 
     // Calculate an expiration date for this order
 
