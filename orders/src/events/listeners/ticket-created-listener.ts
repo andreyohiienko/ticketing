53e5fff7236b88eae44c39ba598c3f56f4fc54b8
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-24 15:31:06 +0300

    Simple onMessage implementation

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,21 @@
 import { Message } from 'node-nats-streaming'
 import { Listener, TicketCreatedEvent, Subjects } from '@aotickets/common'
 import { queueGroupName } from './queue-group-name'
+import { Ticket } from '../../models/ticket'
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated
   queueGropName = queueGroupName
 
-  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { title, price } = data
+
+    const ticket = Ticket.build({
+      title,
+      price,
+    })
+    await ticket.save()
+
+    msg.ack()
+  }
 }
