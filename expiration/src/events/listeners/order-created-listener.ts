e13d93d6bfbfe7ab68c4bb08e01d4af4fa6cb5ad
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-26 13:11:39 +0300

    Delaying job processing

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,9 +8,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGropName = queueGroupName
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    await expirationQueue.add({
-      orderId: data.id,
-    })
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
+    console.log('Waiting this many milliceconds to process the job:', delay)
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      { delay },
+    )
 
     msg.ack()
   }
