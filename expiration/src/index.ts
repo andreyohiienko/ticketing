2f7b7c472fe52908af958f2258e1928d573ccad1
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-26 11:42:28 +0300

    Initial setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,35 @@
+import { natsWrapper } from './nats-wrapper'
+
+const start = async () => {
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('MONGO_CLIENT_ID must be defined')
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URI must be defined')
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    )
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed')
+      process.exit()
+    })
+
+    process.on('SIGINT', () => natsWrapper.client.close()) // INTERRUPT SIGNALS
+    process.on('SIGTERM', () => natsWrapper.client.close()) // TERMINATE SIGNALS
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start()
