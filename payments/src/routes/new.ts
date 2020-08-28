84e007cd310e928b12e8bf8923992309029da1c4
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-28 15:54:59 +0300

    More on publishing

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import { body } from 'express-validator'
 import { Order } from '../models/order'
 import { stripe } from '../stripe'
 import { Payment } from '../models/payment'
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -41,8 +43,13 @@ router.post(
     })
     const payment = Payment.build({ orderId, stripeId: charge.id })
     await payment.save()
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripId,
+    })
 
-    res.status(201).send({ success: true })
+    res.status(201).send({ id: payment.id })
   },
 )
 
